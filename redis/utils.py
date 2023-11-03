5d2cf2b80cae1e73f374d3cd9afca3380c755ea6
李成刚 <lichenggang@uniontech.com>
2023-11-03 18:07:03 +0800

    feat: update python-redis to 4.3.4

--- redis/utils.py
+++ redis/utils.py
@@ -1,14 +1,22 @@
 from contextlib import contextmanager
-
+from typing import Any, Dict, Mapping, Union
 
 try:
     import hiredis  # noqa
+
     HIREDIS_AVAILABLE = True
 except ImportError:
     HIREDIS_AVAILABLE = False
 
+try:
+    import cryptography  # noqa
 
-def from_url(url, db=None, **kwargs):
+    CRYPTOGRAPHY_AVAILABLE = True
+except ImportError:
+    CRYPTOGRAPHY_AVAILABLE = False
+
+
+def from_url(url, **kwargs):
     """
     Returns an active Redis client generated from the given database URL.
 
@@ -16,7 +24,8 @@ def from_url(url, db=None, **kwargs):
     none is provided.
     """
     from redis.client import Redis
-    return Redis.from_url(url, db, **kwargs)
+
+    return Redis.from_url(url, **kwargs)
 
 
 @contextmanager
@@ -26,8 +35,47 @@ def pipeline(redis_obj):
     p.execute()
 
 
-class dummy(object):
+def str_if_bytes(value: Union[str, bytes]) -> str:
+    return (
+        value.decode("utf-8", errors="replace") if isinstance(value, bytes) else value
+    )
+
+
+def safe_str(value):
+    return str(str_if_bytes(value))
+
+
+def dict_merge(*dicts: Mapping[str, Any]) -> Dict[str, Any]:
     """
-    Instances of this class can be used as an attribute container.
+    Merge all provided dicts into 1 dict.
+    *dicts : `dict`
+        dictionaries to merge
     """
-    pass
+    merged = {}
+
+    for d in dicts:
+        merged.update(d)
+
+    return merged
+
+
+def list_keys_to_dict(key_list, callback):
+    return dict.fromkeys(key_list, callback)
+
+
+def merge_result(command, res):
+    """
+    Merge all items in `res` into a list.
+
+    This command is used when sending a command to multiple nodes
+    and the result from each node should be merged into a single list.
+
+    res : 'dict'
+    """
+    result = set()
+
+    for v in res.values():
+        for value in v:
+            result.add(value)
+
+    return list(result)
